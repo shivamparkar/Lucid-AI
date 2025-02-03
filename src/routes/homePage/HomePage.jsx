e5fbde874874f85ee28b0bc99283473ef6bee8bc
Shivam <parkarshivam@gmail.com>
2025-02-03 23:35:34 +0530

    pushing homepage changes

--- src/routes/homePage/HomePage.jsx
+++ src/routes/homePage/HomePage.jsx
@@ -5,8 +5,21 @@ import React from "react";
 
 const Homepage = () => {
   return (
-    <div >
-      <Link to="/dashboard">Dashboard</Link>
+    <div className="homepage">
+      <div className="left">
+        <h1>Luci AI</h1>
+        <h2>Lucid AI – Intelligence Made Clear.</h2>
+        <h3>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit,
+          veniam! Sunt quae porro qui facere, dolores tempore magni sit
+          perspiciatis ad sed illo consectetur quas consequuntur. Excepturi
+          velit distinctio vitae.
+        </h3>
+        <button>
+          Get Started
+        </button>
+      </div>
+      <div className="right"></div>
     </div>
   );
 };
