4f32d300b7913a1d59128f10472dce253cbebc57
Shivam <parkarshivam@gmail.com>
2025-02-08 17:01:18 +0530

    ..

--- src/routes/homePage/HomePage.jsx
+++ src/routes/homePage/HomePage.jsx
@@ -1,29 +1,26 @@
 import { Link } from "react-router-dom";
 import "./homePage.css";
-
-import React, { useState } from "react";
+import { TypeAnimation } from "react-type-animation";
+import { useState } from "react";
 
 import orbital from "../../Images/orbital.png";
 import bot from "../../Images/bot.png";
 import human1 from "../../Images/human1.jpeg";
 import human2 from "../../Images/human2.jpeg";
-
-import { TypeAnimation } from "react-type-animation";
+import logo from "../../Images/logo.png";
 
 const Homepage = () => {
-
-  const[typingStatus, setTypingStatus] = useState("human1")
+  const [typingStatus, setTypingStatus] = useState("human1");
 
   return (
     <div className="homepage">
       <img src={orbital} alt="" className="orbital" />
       <div className="left">
-        <h1>Luci AI</h1>
-        <h2>Lucid AI – Intelligence Made Clear.</h2>
+        <h1>LAMA AI</h1>
+        <h2>Supercharge your creativity and productivity</h2>
         <h3>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit,
-          veniam! Sunt quae porro qui facere, dolores tempore magni sit
-          perspiciatis ad sed illo consectetur quas consequuntur.
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat sint
+          dolorem doloribus, architecto dolor.
         </h3>
         <Link to="/dashboard">Get Started</Link>
       </div>
@@ -34,33 +31,60 @@ const Homepage = () => {
           </div>
           <img src={bot} alt="" className="bot" />
           <div className="chat">
-            <img src={typingStatus === "human1" ? human1 : typingStatus === "human2" ? human2 : bot} alt="" />
+            <img
+              src={
+                typingStatus === "human1"
+                  ? human1
+                  : typingStatus === "human2"
+                  ? human1
+                  : bot
+              }
+              alt=""
+            />
             <TypeAnimation
               sequence={[
-                "Human: We produce food for Mice",
-                1000,() =>{
-                  setTypingStatus('bot')
+                "Human: How do I improve my AI model?",
+                2000,
+                () => {
+                  setTypingStatus("bot");
                 },
-                "Bot: We produce food for Hamsters",
-                1000,() =>{
-                  setTypingStatus('human2')
+                "Bot: You can fine-tune it with domain-specific data for better accuracy.",
+                2000,
+                () => {
+                  setTypingStatus("human2");
                 },
-                "Human: We produce food for Guinea Pigs",
-                1000,() =>{
-                  setTypingStatus('bot')
+                "Human2: What is the best way to handle biases in AI?",
+                2000,
+                () => {
+                  setTypingStatus("bot");
                 },
-                "Bot: We produce food for Chinchillas",
-                1000,() =>{
-                  setTypingStatus('human1')
+                "Bot: Implement diverse datasets and ethical guidelines to reduce biases.",
+                2000,
+                () => {
+                  setTypingStatus("human1");
                 },
               ]}
+              
               wrapper="span"
-              speed={50}
               repeat={Infinity}
               cursor={true}
               omitDeletionAnimation={true}
+              style={{
+                fontSize: "0.7em",
+                backgroundImage: "linear-gradient(to right, #a1cfff, #773bd9)",
+                WebkitBackgroundClip: "text",
+                WebkitTextFillColor: "transparent",
+              }}
             />
           </div>
+        </div>
+      </div>
+      <div className="terms">
+        <img src={logo} alt="" />
+        <div className="links">
+          <Link to="/">Terms of Service</Link>
+          <span>|</span>
+          <Link to="/">Privacy Policy</Link>
         </div>
       </div>
     </div>
